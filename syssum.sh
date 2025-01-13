5a04d20d7c2b045165f489c3e9dcf0da8238607f
Rick Bodnar <66636240+rbodnar75@users.noreply.github.com>
2025-01-13 16:28:54 +0000

    Added IP lookup

--- syssum.sh
+++ syssum.sh
@@ -5,3 +5,6 @@ echo "=====Available Resources======"
 echo Memory usage: $(free -h | awk 'NR==2{print $2" total"}'), $(free -h | awk 'NR==2{print $4" available"}')
 echo Disk usage: $(df -h /dev/sda1 | awk 'NR==2{print $3" used"}'), $(df -h /dev/sda1 | awk 'NR==2{print $4" available"}')
 echo ""
+echo "=====Interface Info==========="
+echo $(ip a | grep eth0)
+echo ""
