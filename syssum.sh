384feb3007652e02b540d1af47b75d9506d1a333
Rick Bodnar <66636240+rbodnar75@users.noreply.github.com>
2025-01-13 16:20:37 +0000

    Fixed typo

--- syssum.sh
+++ syssum.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "System Report for $(uname -n)"
-echo "Available Resources"
-echo Memory usage: $(free -h | awk 'NR==2(print $2" total"}'), $(free -h | awk 'NR==2{print $4" available"}')
-echo Disk usage: $(df -h /dev/sda1 | awk 'NR==2(print $3" used"}'), $(df -h /dev/sda1 | awk 'NR==2{print $4" available"}')
+echo ""
+echo "=====Available Resources======"
+echo Memory usage: $(free -h | awk 'NR==2{print $2" total"}'), $(free -h | awk 'NR==2{print $4" available"}')
+echo Disk usage: $(df -h /dev/sda1 | awk 'NR==2{print $3" used"}'), $(df -h /dev/sda1 | awk 'NR==2{print $4" available"}')
+echo ""
